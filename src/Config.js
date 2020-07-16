fee9cc796a5315471f23dd45ccd2214ed702e9c2
Sijan <48872127+disizme@users.noreply.github.com>
2020-07-16 19:57:13 +0545

    Update Config.js
    config update

--- src/Config.js
+++ src/Config.js
@@ -1,7 +1,7 @@
 export const Config = {
     Title: `Dinemate Admin`,
-    BaseUrl: window.location.host.includes("localhost") ? "http://127.0.0.1:8000/api" : "https://dinemate.com.au/api/api",
-    urlbase: window.location.host.includes("localhost") ? "http://127.0.0.1:8000" : "https://dinemate.com.au",
-    qr_url: "https://dinemate.com.au/customer"
+    BaseUrl: window.location.host.includes("localhost") ? "http://127.0.0.1:8000/api" : "https://dinemate.com.au/Api-dine/api",
+    urlbase: window.location.host.includes("localhost") ? "http://127.0.0.1:8000" : "https://dinemate.com.au/Api-dine",
+    qr_url: "https://dinemate.com.au/customer-dine"
 }
 
