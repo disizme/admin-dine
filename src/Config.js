eaec3ca74ba993237fe0bb5e5ac319e747c43fb4
Sijan <48872127+disizme@users.noreply.github.com>
2020-07-18 20:36:13 +0545

    Update Config.js

--- src/Config.js
+++ src/Config.js
@@ -1,7 +1,7 @@
 export const Config = {
     Title: `Dinemate Admin`,
-    BaseUrl: window.location.host.includes("localhost") ? "http://127.0.0.1:8000/api" : "http://172.105.191.214/Api-dine/api",
-    urlbase: window.location.host.includes("localhost") ? "http://127.0.0.1:8000" : "http://172.105.191.214/Api-dine",
+    BaseUrl: window.location.host.includes("localhost") ? "http://127.0.0.1:8000/api" : "http://172.105.191.214/api",
+    urlbase: window.location.host.includes("localhost") ? "http://127.0.0.1:8000" : "http://172.105.191.214",
     qr_url: "http://172.105.191.214/customer-dine"
 }
 
