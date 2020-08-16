5877f900af69e3de68b3c8ccfb900e4152a2da70
Sijan Shrestha <dsijan95@gmail.com>
2020-08-16 21:37:06 +0545

    For ssl test

--- src/Config.js
+++ src/Config.js
@@ -1,7 +1,7 @@
 export const Config = {
     Title: `Dinemate App`,
-    BaseUrl: window.location.host.includes("localhost") ? "http://127.0.0.1:8000/api" : `https://api.dinemate.com.au/api`,
-    urlbase: window.location.host.includes("localhost") ? "http://127.0.0.1:8000/media/" : "https://api.dinemate.com.au/media/", 
+    BaseUrl: window.location.host.includes("localhost") ? "http://127.0.0.1:8000/api" : `https://mock-api.dinemate.com.au/api`,
+    urlbase: window.location.host.includes("localhost") ? "http://127.0.0.1:8000/media/" : "https://mock-api.dinemate.com.au/media/", 
     // BaseUrl: `https://api.dinemate.com.au/api`,
     // urlbase: `https://api.dinemate.com.au/media/`,
     // qr_url: window.location.host.includes("localhost") ? "http://localhost:2001" : "https://customer.dinemate.com.au", 
